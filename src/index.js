03e1d07a96ce1c8d2ebf1d502edd3a188eb13961
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-11-01 23:10:27 +0530

    feat : added basic self focus component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-class SelfFocus extends PureComponent {}
+const defaultProps = {
+	tag: 'div',
+	className: '',
+	tabIndex: 0
+};
+
+const propTypes = {
+	tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+	className: PropTypes.string,
+	tabIndex: PropTypes.number
+};
+
+class SelfFocus extends React.Component {
+	componentDidMount() {
+		this.focusTag.focus();
+	}
+	render() {
+		let { className, tag: Tag, children, tabIndex = -1, ...rest } = this.props;
+		return (
+			<Tag
+				{...rest}
+				ref={focusTag => {
+					this.focusTag = focusTag;
+				}}
+				tabIndex={tabIndex}
+				className={className}>
+				{children}
+			</Tag>
+		);
+	}
+}
+
+SelfFocus.propTypes = propTypes;
+SelfFocus.defaultProps = defaultProps;
+
 export default SelfFocus;
